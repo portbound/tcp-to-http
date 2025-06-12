6885d4db61b4ff9d051f824175720da37fd25822
portbound <prtbnd@proton.me>
2025-06-11 22:20:29 -0400

    CH2 (moved to TCP conn from reading file)

--- main.go
+++ main.go
@@ -4,27 +4,70 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"os"
+	"log"
+	"net"
+	"strings"
 )
 
+const port = ":42069"
+
 func main() {
-	filePath := "foo"
-	file, err := os.Open(filePath)
+	listener, err := net.Listen("tcp", port)
 	if err != nil {
-		panic(err)
+		log.Fatalf("error: failed to listen to TCP traffic: %s", err)
 	}
-	defer file.Close()
+	defer listener.Close()
 
 	for {
-		buffer := make([]byte, 8)
-		n, err := file.Read(buffer)
+		conn, err := listener.Accept()
 		if err != nil {
-			if errors.Is(err, io.EOF) {
-				break
-			}
-			fmt.Printf("error: %s\n", err.Error())
+			log.Fatalf("error: failed to accept conn => %s", err)
 		}
-		s := string(buffer[:n])
-		fmt.Printf("read: %s\n", s)
+
+		fmt.Printf("Accepted connection from %s\n", conn.RemoteAddr())
+
+		for line := range getLinesChannel(conn) {
+			fmt.Println(line)
+		}
+		fmt.Printf("Connection to %s closed\n", conn.RemoteAddr())
 	}
+}
+
+func getLinesChannel(conn io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		defer conn.Close()
+		defer close(ch)
+
+		line := ""
+		buffer := make([]byte, 8)
+
+		for {
+			n, err := conn.Read(buffer)
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				fmt.Printf("error: %s\n", err.Error())
+			}
+
+			line += string(buffer[:n])
+
+			parts := strings.Split(line, "\n")
+
+			// each part except the last is guaranteed end on a '\n'
+			// if the last part happens to end on '\n' it will be caught in next iteration nbd
+			for _, part := range parts[:len(parts)-1] {
+				ch <- part
+			}
+			line = parts[len(parts)-1]
+		}
+
+		// send any remaining text that didn't fill the buffer by 8 bytes and didn't end in a new line
+		if line != "" {
+			ch <- line
+		}
+	}()
+	return ch
 }
