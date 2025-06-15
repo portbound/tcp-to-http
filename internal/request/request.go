aa7dbb80257c6e62cee718de5babc51be8582d97
portbound <prtbnd@proton.me>
2025-06-14 23:45:28 -0400

    CH4 (added stream parsing)
    most of the logic is here but there are some bugs
    
    squashed w/
    CH4 DONE (added stream parsing)

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,74 +1,106 @@
 package request
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
 	"io"
 	"strings"
 	"unicode"
 )
 
-// GET /coffee HTTP/1.1
-// Host: localhost:42069
-// User-Agent: curl/7.81.0
-// Accept: */*
-
-// {"flavor":"dark mode"}
+const bufferSize = 8
+const (
+	stateinit = iota
+	stateDone
+)
 
 type Request struct {
 	RequestLine RequestLine
+	State       int
 }
-
 type RequestLine struct {
 	HttpVersion   string
 	RequestTarget string
 	Method        string
 }
 
+func (r *Request) parse(data []byte) (int, error) {
+	if r.State == stateinit {
+		return parseRequestLine(r, data)
+	}
+
+	if r.State == stateDone {
+		return 0, fmt.Errorf("error: trying to read data in a done state")
+	}
+
+	return 0, fmt.Errorf("error: unknown state")
+}
+
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	req := Request{}
-
-	data, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read from request: %w", err)
+	buf := make([]byte, bufferSize, bufferSize)
+	readToIndex := 0
+	req := Request{
+		State: stateinit,
 	}
 
-	rawRequest := strings.Split(string(data), "\r\n")
+	for req.State != stateDone {
+		if readToIndex == cap(buf) {
+			tmp := make([]byte, cap(buf)*2)
+			copy(tmp, buf[:readToIndex])
+			buf = tmp
+		}
 
-	if len(rawRequest) == 0 {
-		return nil, fmt.Errorf("invalid request, missing Request Line")
+		bytesRead, err := reader.Read(buf[readToIndex:])
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				req.State = 1
+				break
+			}
+			return nil, err
+		}
+
+		readToIndex += bytesRead
+
+		bytesParsed, err := req.parse(buf[:readToIndex])
+		if err != nil {
+			return nil, err
+		}
+
+		copy(buf, buf[bytesParsed:readToIndex])
+		readToIndex -= bytesParsed
 	}
-
-	reqLine, err := parseRequestLine(rawRequest[0])
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse request line: %w", err)
-	}
-
-	req.RequestLine = *reqLine
 	return &req, nil
 }
 
-func parseRequestLine(data string) (*RequestLine, error) {
-	fields := strings.Split(data, " ")
-
-	if len(fields) != 3 {
-		return nil, fmt.Errorf("invalid Request Line. Expected 3 parts, got %d", len(fields))
+func parseRequestLine(r *Request, data []byte) (int, error) {
+	if !bytes.Contains(data, []byte{'\r', '\n'}) {
+		return 0, nil
 	}
 
-	reqLine := RequestLine{
+	data = bytes.TrimSuffix(data, []byte{'\r', '\n'})
+	fields := strings.Split(string(data), " ")
+
+	if len(fields) != 3 {
+		return 0, fmt.Errorf("invalid Request Line. Expected 3 parts, got %d", len(fields))
+	}
+
+	if fields[2] != "HTTP/1.1" {
+		return 0, fmt.Errorf("only HTTP/1.1 is supported. Got=%s", fields[2])
+	}
+
+	for _, char := range fields[0] {
+		if !unicode.IsUpper(char) {
+			return 0, fmt.Errorf("invalid HTTP method. Got=%s", fields[0])
+		}
+	}
+
+	r.State = stateDone
+	r.RequestLine = RequestLine{
 		HttpVersion:   fields[2],
 		RequestTarget: fields[1],
 		Method:        fields[0],
 	}
 
-	if reqLine.HttpVersion != "HTTP/1.1" {
-		return nil, fmt.Errorf("only HTTP/1.1 is supported. Got=%s", reqLine.HttpVersion)
-	}
-
-	for _, char := range reqLine.Method {
-		if !unicode.IsUpper(char) {
-			return nil, fmt.Errorf("invalid HTTP method. Got=%s", reqLine.Method)
-		}
-	}
-
-	return &reqLine, nil
+	return len(data), nil
 }
