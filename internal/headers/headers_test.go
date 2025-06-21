ff013913427ef547b30760b1d19263730082709a
portbound <prtbnd@proton.me>
2025-06-20 22:58:52 -0400

    CH5 added new tests for header parsing

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -18,13 +18,61 @@ func TestHeadersParse(t *testing.T) {
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, []string{"localhost:42069"}, headers["host"])
 	assert.Equal(t, 23, n)
 	assert.False(t, done)
+
+	// Test: Valid single header with extra whitespace
+	headers = NewHeaders()
+	data = []byte("       Host: localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, []string{"localhost:42069"}, headers["host"])
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
+
+	// Test: Valid 2 headers with existing headers
+	headers = NewHeaders()
+	headers["host"] = []string{"localhost:42069"}
+	data = []byte("Content-Type: application/json\r\nCache-Control: max-age=604800\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, []string{"localhost:42069"}, headers["host"])
+	assert.Equal(t, []string{"application/json"}, headers["content-type"])
+	assert.False(t, done)
+
+	// Test: Valid append header
+	headers = NewHeaders()
+	headers["set-person"] = []string{"lane-loves-go", "prime-loves-zig", "tj-loves-ocaml"}
+	data = []byte("set-person: jake-loves-vim\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, []string{"lane-loves-go", "prime-loves-zig", "tj-loves-ocaml", "jake-loves-vim"}, headers["set-person"])
+	assert.False(t, done)
+
+	// Test: Valid done
+	headers = NewHeaders()
+	data = []byte("\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, 2, n)
+	assert.True(t, done)
 
 	// Test: Invalid spacing header
 	headers = NewHeaders()
 	data = []byte("       Host : localhost:42069       \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+
+	//Test: Invalid char in field-name
+	headers = NewHeaders()
+	data = []byte("H©st: localhost:42069\r\n\r\n")
 	n, done, err = headers.Parse(data)
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
